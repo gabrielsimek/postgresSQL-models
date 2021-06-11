69dde5e32eecbc4190cfea782aa2402d379e1dfb
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-11 05:51:23 -0700

    put router added and pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -96,15 +96,33 @@ describe('/api/v1/pokemon,', () => {
   });
 
   it('gets all pokemon from /api/v1/pokemon', async () => {
-    const pika = await Pokemon.insert({ name: 'pikachu', typeOne: 'electric', typeTwo: 'NA', attack: 55 });
-    const char = await Pokemon.insert({ name: 'charmeleon', typeOne: 'fire', typeTwo: 'NA', attack: 64 });
-    const bulba = await Pokemon.insert({ name: 'bulbasaur', typeOne: 'grass', typeTwo: 'poison', attack: 49 });
+    const pika = { name: 'pikachu', typeOne: 'electric', typeTwo: 'NA', attack: 55 };
+    const char = { name: 'charmeleon', typeOne: 'fire', typeTwo: 'NA', attack: 64 };
+    const bulba = { name: 'bulbasaur', typeOne: 'grass', typeTwo: 'poison', attack: 49 };
+    const pokemon = [pika, char, bulba];
+    const expected = await Promise.all(
+      pokemon.map(poke => {
+        return  Pokemon.insert(poke);
+      }));
 
     const res = await request(app)
       .get('/api/v1/pokemon');
 
     expect(res.status).toBe(200);
-    expect(res.body).toEqual([pika, char, bulba]);
+    expect(res.body).toEqual(expected);
+  });
+  //!!!finish update
+  it('updates a pokemon at /api/v1/pokemon/:id', async () => {
+    const pokemon = await Pokemon.insert({ name: 'pikachu', typeOne: 'electric', typeTwo: 'NA', attack: 55 });
+    pokemon.attack = 5000;
+    pokemon.typeTwo = 'dragon';
+    console.log(pokemon);
+    const res = await request(app)
+      .put(`/api/v1/pokemon/${pokemon.id}`)
+      .send(pokemon);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(pokemon);
   });
   it('gets a removes a pokemon from /api/v1/pokemon:id', async () => {
     const char = await Pokemon.insert({ name: 'charmeleon', typeOne: 'fire', typeTwo: 'NA', attack: 64 });
