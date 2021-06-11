54f097f862ce86f2cbb8b5de88f65f01889f2f01
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-10 21:29:20 -0700

    MC getById pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Motorcycle from '../lib/models/Motorcycle';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -12,8 +13,18 @@ describe('demo routes', () => {
     const res = await request(app)
       .post('/api/v1/motorcycles')
       .send({ make: 'Kawasaki', model: 'Ninja-650', horsepower: 310 });
-    console.log(res.body);
+
     expect(res.status).toBe(200);
     expect(res.body).toEqual({ id: '1', make: 'Kawasaki', model: 'Ninja-650', horsepower: 310 });
   });
+  it('gets a single motorcycle specified by id from db', async () => {
+    const motorcycle = await Motorcycle.insert({ make: 'Kawasaki', model: 'Ninja-650', horsepower: 310 });
+    
+    const res = await request(app)
+      .get(`/api/v1/motorcycles${motorcycle.id}`);
+      
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(motorcycle);
+  });
+  
 });
