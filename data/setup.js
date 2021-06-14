d2475e64ffc40b74ababa5d1cc096078e870432a
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-14 10:15:22 -0700

    setup.js updated to pass CI

--- data/setup.js
+++ data/setup.js
@@ -1,15 +1,30 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import { promises as fs } from 'fs';
-
-
-export default async (pool) => {
-  const sql = await fs
+// import path from 'path';
+export default (pool) => {
+  return fs
     .readFile(
-      'C:/Users/gabri/alchemy/career-track/postgresSQL-models/sql/setup.sql',
+      './sql/setup.sql',
       {
         encoding: 'utf-8',
       }
-    );
-  return pool.query(sql);
+    )
+    .then((sql) => pool.query(sql));
 };
+
+
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import { promises as fs } from 'fs';
+
+
+// export default async (pool) => {
+//   const sql = await fs
+//     .readFile(
+//       'C:/Users/gabri/alchemy/career-track/postgresSQL-models/sql/setup.sql',
+//       {
+//         encoding: 'utf-8',
+//       }
+//     );
+//   return pool.query(sql);
+// };
